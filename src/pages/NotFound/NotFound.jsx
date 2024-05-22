9c81b1c1d53610471091d9dbb5d178992bc7ac7d
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-22 10:57:16 +0300

    update not found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,19 @@
+import { TbFaceIdError } from "react-icons/tb";
+import { NavLink } from "react-router-dom";
+import s from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <TbFaceIdError className={s.icon} size="100" />
+        <h1 className={s.title}>Oops, such page does not exist</h1>
+        <NavLink className={s.link} to="/">
+          Log in
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
